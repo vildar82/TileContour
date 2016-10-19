68bb6174e7f747957df2468cd6d9480f159d7734
Vildar Khisyametdinov <vildar82@gmail.com>
2016-10-19 16:24:19 +0300

    statrt command

--- TileContour/Commands.cs
+++ TileContour/Commands.cs
@@ -10,52 +10,54 @@ namespace vil.acad.TileContour
 {
    public static class Commands
    {
-      [CommandMethod("TileContour")]
-      public static void TileContour()
-      {
-         Document doc = Application.DocumentManager.MdiActiveDocument;
-         Database db = doc.Database;
-         Editor ed = doc.Editor;
+        [CommandMethod("TileContour")]
+        public static void TileContour ()
+        {
+            AcadLib.CommandStart.Start((doc) =>
+            {
+                Database db = doc.Database;
+                Editor ed = doc.Editor;
 
-         // Jig отрисовки полилинии по задаваемым точкам.
-         TileLineJig jigTest = new TileLineJig();
-         PromptResult jigRes;
-         bool status = true;
-         do
-         {
-            jigRes = ed.Drag(jigTest);
-            if (jigRes.Status == PromptStatus.OK)
-            {
-               // Добавление указанной точки
-               jigTest.AddNewPromptPoint();
-            }
-            else if (jigRes.Status == PromptStatus.Cancel || jigRes.Status == PromptStatus.Error)
-            {
-               return;
-            }
-            else if (jigRes.Status == PromptStatus.Other)
-            {
-               status = false;
-            }
-         } while (status);
+                // Jig отрисовки полилинии по задаваемым точкам.
+                TileLineJig jigTest = new TileLineJig();
+                PromptResult jigRes;
+                bool status = true;
+                do
+                {
+                    jigRes = ed.Drag(jigTest);
+                    if (jigRes.Status == PromptStatus.OK)
+                    {
+                        // Добавление указанной точки
+                        jigTest.AddNewPromptPoint();
+                    }
+                    else if (jigRes.Status == PromptStatus.Cancel || jigRes.Status == PromptStatus.Error)
+                    {
+                        return;
+                    }
+                    else if (jigRes.Status == PromptStatus.Other)
+                    {
+                        status = false;
+                    }
+                } while (status);
 
-         // Добавление полилинии в чертеж.
-         using (Transaction tr = db.TransactionManager.StartTransaction())
-         {
-            BlockTableRecord btr = (BlockTableRecord)tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite);
-            Autodesk.AutoCAD.DatabaseServices.Polyline pl = new Autodesk.AutoCAD.DatabaseServices.Polyline();
-            pl.SetDatabaseDefaults();
-            for (int i = 0; i < jigTest.AllVertex.Count; i++)
-            {
-               Point3d pt3d = jigTest.AllVertex[i];
-               Point2d pt2d = new Point2d(pt3d.X, pt3d.Y);
-               pl.AddVertexAt(i, pt2d, 0, db.Plinewid, db.Plinewid);
-            }
-            pl.TransformBy(jigTest.UCS);
-            btr.AppendEntity(pl);
-            tr.AddNewlyCreatedDBObject(pl, true);
-            tr.Commit();
-         }
-      }
+                // Добавление полилинии в чертеж.
+                using (Transaction tr = db.TransactionManager.StartTransaction())
+                {
+                    BlockTableRecord btr = (BlockTableRecord)tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite);
+                    Autodesk.AutoCAD.DatabaseServices.Polyline pl = new Autodesk.AutoCAD.DatabaseServices.Polyline();
+                    pl.SetDatabaseDefaults();
+                    for (int i = 0; i < jigTest.AllVertex.Count; i++)
+                    {
+                        Point3d pt3d = jigTest.AllVertex[i];
+                        Point2d pt2d = new Point2d(pt3d.X, pt3d.Y);
+                        pl.AddVertexAt(i, pt2d, 0, db.Plinewid, db.Plinewid);
+                    }
+                    pl.TransformBy(jigTest.UCS);
+                    btr.AppendEntity(pl);
+                    tr.AddNewlyCreatedDBObject(pl, true);
+                    tr.Commit();
+                }
+            });
+        }
    }  
 }
